9e32dbaf674767991fc3b001053761d202b5f871
garycunningham89 <47434962+garycunningham89@users.noreply.github.com>
2021-01-07 00:52:18 +0000

    Updating Webservice.py

--- webservice.py
+++ webservice.py
@@ -1,5 +1,4 @@
 from flask import Flask, url_for, request, redirect, abort, jsonify, session
-
 app = Flask(__name__,
             static_url_path='', 
             static_folder='staticpages')
@@ -8,35 +7,23 @@ app.secret_key = 'SecretMachineL'
 
 #index
 @app.route('/')
-def index():
-    #return "hello"
-    count=0
-    count+=1
-
-    if not 'counter' in session:
-        session['counter'] =0
-        print("new session")
-
-    sessionCount=session['counter']
-    sessionCount+=1
-    session['counter']=sessionCount
-
+def home():
+    return app.send_static_file('index.html')
     
-
-    pageContent="<h1>counts</h1>" +\
-        "session Count ="+str(sessionCount) +\
-        "<br/>this Count ="+str(count)
-    
-    return pageContent
-    
-@app.route('/clear')
-def clear():
-    #session.clear()
-    session.pop('counter',None)   
-
-    return "done"
-    
+@app.route('/api/keras')
+def keras():
+    return {"value": kmodel()}
 #curl http://127.0.0.1:5000
+
+@app.route('/api/sklearn')
+def sklearn():
+    return {"value": skmodel()}
+
+def kmodel():
+    return 'Hello World!'
+
+def skmodel():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.debug = True
